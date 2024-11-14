85778949bdb5aa76f000ccf5988139cc92b8f73c
ElekJTT <113401622+ElekJTT@users.noreply.github.com>
2024-11-14 12:21:06 -0600

    Update MockDataBaseCreator.py

--- MockDataBaseCreator.py
+++ MockDataBaseCreator.py
@@ -2,23 +2,29 @@ import psycopg2
 
 def main():
 
-    conn = psycopg2.connect(database = "mock_data", 
-                            user = "aicomps", 
-                            host= 'localhost',
-                            password = "12345",
-                            port = 5432)
+        conn = psycopg2.connect(database = "mock_data", 
+                                user = "aicomps", 
+                                host= 'localhost',
+                                password = "12345",
+                                port = 5432)
 
 
-    cur = conn.cursor()
+        cur = conn.cursor()
 
-    cur.execute("""CREATE TABLE mock_data(
-            game_num SERIAL PRIMARY KEY,
-            course_name VARCHAR (50) UNIQUE NOT NULL,
-            course_instructor VARCHAR (100) NOT NULL,
-            topic VARCHAR (20) NOT NULL);
-            """)
-    # Make the changes to the database persistent
-    conn.commit()
-    # Close cursor and communication with the database
-    cur.close()
-    conn.close()
+        #create a table
+        cur.execute("""CREATE TABLE mock_data(
+                game_num SERIAL PRIMARY KEY, XXXXXXXX, XXXXXXXX, XXXXXX)""")
+
+
+        #import some mock games
+        inputString = """INSERT INTO mock_data (ID, XXXXXXXXXX,XXXXXXX) VALUES(%s)"""
+        arrarOfMockGames = []
+        for game in arrarOfMockGames:
+                cur.execute(inputString, game)
+
+
+        # Make the changes to the database persistent
+        conn.commit()
+        # Close cursor and communication with the database
+        cur.close()
+        conn.close()
