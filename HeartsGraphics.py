bcd8686c02749f88c58ec693d5222b8e799cadb2
ElekJTT <113401622+ElekJTT@users.noreply.github.com>
2025-02-25 09:54:02 -0600

    Small edits

--- HeartsGraphics.py
+++ HeartsGraphics.py
@@ -59,7 +59,7 @@ class Hearts:
 			self.players = [ElainaAgent("Elaina", auto=False, game=self.myGame), Strong_agent("Elek", auto=True), Player("MarySue", auto=True), Player("Monte", auto=True)]
 		else:
 			# Make four players
-			self.players = [Elek_Agent("Elek", auto=True), BreannaAgent("Breanna", auto=True), Cbr_Agent("MarySue", auto=True), Strong_agent("Monte", auto=True)]
+			self.players = [Elek_Agent("Elek", auto=True), BreannaAgent("Breanna", auto=True), Player("MarySue", auto=True), Strong_agent("Monte", auto=True)]
   
 		'''
 		Player physical locations:
