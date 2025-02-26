0ce40e75dd53fbb81fd3bbfc00fe1ece67502951
lefeversb <lefeversb@carleton.edu>
2025-02-26 16:52:35 -0600

    adding equal sign

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -413,7 +413,7 @@ class MonteCarlo(Player):
             
             childTrump = str(child.board[1])[-1]
             
-            if(self.trickNum != 1): #will consider everything but hearts and the queen of spades on first trick)
+            if(self.trickNum == 1): #will consider everything but hearts and the queen of spades on first trick)
                 if(str(child.board[1]) != "h"): #if not a heart
                    if(str(child.board[1]) != "Qs"): #or queen of spades on the first trick, consider it
                         if(childTrump == self.curTrump):
